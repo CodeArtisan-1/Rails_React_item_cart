dec814def24799c51deda84f38598772b8c1e587
Berestov <berestov.vych@yandex.com>
2017-08-02 02:44:55 +0300

    react create and read items initial version

---
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -0,0 +1,26 @@
+var AllItems = React.createClass({
+	componentDidMount() {
+		$.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
+	},
+
+	getInitialState() {
+		return { items: [] }
+	},
+
+	render() {
+		var items = this.state.items.map((item) => {
+			return (
+				<div>
+					<h3>{ item.name }</h3>
+					<p>{ item.description }</p>
+				</div>
+			)
+		});
+
+		return (
+			<div>
+				{ items }
+			</div>
+		)
+	}
+});
